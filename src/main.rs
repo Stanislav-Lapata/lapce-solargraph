bbf3449c076b514c4dab61448f2b8f148de965fc
Stanislav Lapata <stanislavlapata@gmail.com>
2022-12-26 01:58:29 +0100

    add default options

--- src/main.rs
+++ src/main.rs
@@ -19,78 +19,45 @@ fn initialize(params: InitializeParams) -> Result<()> {
         pattern: Some(String::from("**/*.rb")),
         scheme: None,
     }];
-    let mut server_args = vec![];
+    let mut server_args = vec![String::from("stdio")];
 
     if let Some(options) = params.initialization_options.as_ref() {
-        if let Some(lsp) = options.get("lsp") {
-            if let Some(args) = lsp.get("serverArgs") {
-                if let Some(args) = args.as_array() {
-                    if !args.is_empty() {
-                        server_args = vec![];
-                    }
-                    for arg in args {
-                        if let Some(arg) = arg.as_str() {
-                            server_args.push(arg.to_string());
-                        }
+        if let Some(args) = options.get("serverArgs") {
+            if let Some(args) = args.as_array() {
+                if !args.is_empty() {
+                    server_args = vec![];
+                }
+                for arg in args {
+                    if let Some(arg) = arg.as_str() {
+                        server_args.push(arg.to_string());
                     }
                 }
             }
+        }
 
-            if let Some(server_path) = lsp.get("serverPath") {
-                if let Some(server_path) = server_path.as_str() {
-                    if !server_path.is_empty() {
-                        let server_uri = Url::parse(&format!("urn:{}", server_path))?;
-                        PLUGIN_RPC.start_lsp(
-                            server_uri,
-                            server_args,
-                            document_selector,
-                            params.initialization_options,
-                        );
-                        return Ok(());
-                    }
+        if let Some(server_path) = options.get("serverPath") {
+            if let Some(server_path) = server_path.as_str() {
+                if !server_path.is_empty() {
+                    let server_uri = Url::parse(&format!("urn:{}", server_path))?;
+                    PLUGIN_RPC.start_lsp(
+                        server_uri,
+                        server_args,
+                        document_selector,
+                        params.initialization_options,
+                    );
+                    return Ok(());
                 }
             }
         }
     }
 
-    // Architecture check
-    let _ = match VoltEnvironment::architecture().as_deref() {
-        Ok("x86_64") => "x86_64",
-        Ok("aarch64") => "aarch64",
-        _ => return Ok(()),
+    let server_uri = match VoltEnvironment::operating_system().as_deref() {
+        Ok("windows") => return Ok(()),
+        _ => Url::parse("urn:typescript-language-server")?,
     };
 
-    // OS check
-    let _ = match VoltEnvironment::operating_system().as_deref() {
-        Ok("macos") => "macos",
-        Ok("linux") => "linux",
-        Ok("windows") => "windows",
-        _ => return Ok(()),
-    };
-
-    // Download URL
-    // let _ = format!("https://github.com/<name>/<project>/releases/download/<version>/{filename}");
-
-    // see lapce_plugin::Http for available API to download files
-
-    let _ = match VoltEnvironment::operating_system().as_deref() {
-        Ok("windows") => {
-            format!("{}.exe", "[filename]")
-        }
-        _ => "[filename]".to_string(),
-    };
-
-    // Plugin working directory
-    let volt_uri = VoltEnvironment::uri()?;
-    let server_path = Url::parse(&volt_uri)?.join("[filename]")?;
-
-    // if you want to use server from PATH
-    // let server_path = Url::parse(&format!("urn:{filename}"))?;
-
-    // Available language IDs
-    // https://github.com/lapce/lapce/blob/HEAD/lapce-proxy/src/buffer.rs#L173
     PLUGIN_RPC.start_lsp(
-        server_path,
+        server_uri,
         server_args,
         document_selector,
         params.initialization_options,
